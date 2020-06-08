981ba9086d09656a50e4d83efadc7051fb5eb241
Maksym Derbenev <dereban25@gmail.com>
2020-06-08 11:23:31 +0300

    new solution

--- JavaScript/Lesson_4/task_3/index.js
+++ JavaScript/Lesson_4/task_3/index.js
@@ -1,6 +1,6 @@
 for (let n = 1; n <= 5; n++) {
+	console.log('=> ' + n + ' <=');
 	for (let m = 1; m <= 10; m++) {
 		console.log(n + ' x ' + m + ' = ' + n * m)
 	}
-	console.log('=> ' + n + ' <=');
 }

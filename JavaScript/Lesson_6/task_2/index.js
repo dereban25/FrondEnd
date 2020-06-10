776b8bffe42f1f50c9a5fc7550d5c154bfbcddc3
Maksym Derbenev <dereban25@gmail.com>
2020-06-10 12:52:58 +0300

    new solution

--- JavaScript/Lesson_6/task_2/index.js
+++ JavaScript/Lesson_6/task_2/index.js
@@ -1,5 +1,12 @@
-function sum(a,b){
-	console.log(a +b);
-	return a + b;
+let sumOfNumbers = 0;
+
+function getSum(arr) {
+	if (!Array.isArray(arr)) {
+		return null;
+	}
+	for (let i = 0; i < arr.length; i++) {
+		sumOfNumbers += arr[i];
+	}
+	return sumOfNumbers;
 }
-// sum(5,2); 
+// console.log(getSum([10,10,20]));

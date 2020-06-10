8a6a6b700f667fafd95f87ffc3687b6ac66e16b3
Maksym Derbenev <dereban25@gmail.com>
2020-06-10 19:45:36 +0300

    new solution

--- JavaScript/Lesson_6/task_6/index.js
+++ JavaScript/Lesson_6/task_6/index.js
@@ -1,5 +1,11 @@
-function sum(a,b){
-	console.log(a +b);
-	return a + b;
+function squareArray(arr){
+	let arrInSquare= [];
+	if (!Array.isArray(arr)){
+		return null;
+	}
+	for(let i=0;i<arr.length;i++){
+		arrInSquare.push(Math.pow(arr[i],2));
+	}
+	return arrInSquare;
 }
-// sum(5,2); 
+

b5dc085cc977df35853a26a452c69c94ececc095
Maksym Derbenev <dereban25@gmail.com>
2020-06-11 11:44:57 +0300

    new solution

--- JavaScript/Lesson_6/task_14/index.js
+++ JavaScript/Lesson_6/task_14/index.js
@@ -1,5 +1,9 @@
-function sum(a,b){
-	console.log(a +b);
-	return a + b;
+function getSubArray(arr,n) {
+	let newArray = [];
+	for (let i = 0; i < n;i++){
+		newArray[i]=arr[i];
+	}
+	return newArray;
 }
-// sum(5,2); 
+
+// console.log(getSubArray([10,20,30,45],3));

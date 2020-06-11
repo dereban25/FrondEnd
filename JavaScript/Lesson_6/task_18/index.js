bc387fcb6ea92d063c9959159f80d60055c8a61b
Maksym Derbenev <dereban25@gmail.com>
2020-06-11 12:56:20 +0300

    new

--- JavaScript/Lesson_6/task_18/index.js
+++ JavaScript/Lesson_6/task_18/index.js
@@ -1,10 +1,8 @@
-
-// eslint-disable-next-line no-unused-vars
 function run() {
-	let a = 0,
-		b = 1;
-	
-	if (a == b) {
-		return c;
-	}
+    var a = 0,
+        b = 1;
+
+    if (a == b) {
+        return c;
+    }
 }

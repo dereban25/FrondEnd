a88f9471ef9af1df1ced0ceba58003db90dce985
Maksym Derbenev <dereban25@gmail.com>
2020-06-11 11:38:28 +0300

    new solution

--- JavaScript/Lesson_6/task_13/index.js
+++ JavaScript/Lesson_6/task_13/index.js
@@ -1,5 +1,13 @@
-function sum(a,b){
-	console.log(a +b);
-	return a + b;
+function withdraw(clients,balances,client,amount) {
+ for (let i = 0; i < clients.length; i++){
+ 	if (clients[i] === client){
+ 		if(amount > balances[i]){
+ 			return -1;
+		}
+ 		balances[i]-=amount;
+ 		return balances[i];
+	}
+ }
 }
-// sum(5,2); 
+
+// console.log(withdraw(["Max","Peter","Nata"],[200,300,125],"Peter",350));

f740e187e50e90f2964aa8274f1fde5019b4166c
Maksym Derbenev <dereban25@gmail.com>
2020-06-11 10:46:50 +0300

    new solution

--- JavaScript/Lesson_6/task_11/index.js
+++ JavaScript/Lesson_6/task_11/index.js
@@ -1,5 +1,20 @@
-function sum(a,b){
-	console.log(a +b);
-	return a + b;
+function checker(arr) {
+	if (!Array.isArray(arr)) {
+		return null;
+	}
+	if (Array.isArray(arr)) {
+		let min = arr[0];
+		for (let i = 0; i <= arr.length; i++) {
+			if (min > arr[i]) {
+				min = arr[i];
+			}
+		}
+		let max = arr[0];
+		for (let i = 0; i < arr.length; i++) {
+			if (max < arr[i]) {
+				max = arr[i];
+			}
+		}
+		return (max+min)>1000?true:false;
+	}
 }
-// sum(5,2); 

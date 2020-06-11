70b11605e7eedb00f7c8d376726a88ae1f7edd33
Maksym Derbenev <dereban25@gmail.com>
2020-06-11 10:32:57 +0300

    new solution

--- JavaScript/Lesson_6/task_10/index.js
+++ JavaScript/Lesson_6/task_10/index.js
@@ -1,5 +1,12 @@
-function sum(a,b){
-	console.log(a +b);
-	return a + b;
+function cloneArr(arr) {
+	if (!Array.isArray(arr)) {
+		return null;
+	}
+	if (Array.isArray(arr)) {
+		let new_arr = [];
+		for (let i = 0; i <= arr.length - 1; i++) {
+			new_arr.push(arr[i]);
+		}
+		return new_arr;
+	}
 }
-// sum(5,2); 

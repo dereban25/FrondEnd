bb32d01eb1cf4b3a2238b02468731f81b3e98260
Maksym Derbenev <dereban25@gmail.com>
2020-06-17 12:41:36 +0300

    new solution

---
+++ JavaScript/Lesson_10/task_6/index.js
@@ -0,0 +1,6 @@
+const getRandomNumbers = (length, start, end) => {
+	if (Math.floor(start) >= Math.floor(end))
+		return null;
+	return Array(length).fill().map(num =>
+		Math.floor(Math.random() * (start - end + 1) + end));
+};

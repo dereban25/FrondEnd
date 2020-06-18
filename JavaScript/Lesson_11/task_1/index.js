e9b0c7cf850425564499cbf41631cba4728fe444
Maksym Derbenev <dereban25@gmail.com>
2020-06-18 11:50:50 +0300

    new solution

--- JavaScript/Lesson_11/task_1/index.js
+++ JavaScript/Lesson_11/task_1/index.js
@@ -1,18 +1,21 @@
-const splitText = (text, num) => {
-	if (typeof text !== "string")
+const splitText = (text, len = 10) => {
+	
+	if (typeof text != 'string')
 		return null;
 	
 	const strArr = [];
-	let startText = 0;
+	let start = 0;
 	
 	while (true) {
-		const chunk = text.substr(startText, num);
-		if (chunk.length === 0) {
+		let chunk = text.substr(start, len);
+		if (chunk.length === 0)
 			break;
-		}
+		
 		strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
-		startText += num;
+		start += len;
 	}
+	
+	
 	return strArr.join('\n');
 };
 

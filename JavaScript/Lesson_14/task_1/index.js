271212fc1900eb39f74dc87c545cca8684839f73
Maksym Derbenev <dereban25@gmail.com>
2020-06-23 11:02:17 +0300

    new solution

---
+++ JavaScript/Lesson_14/task_1/index.js
@@ -0,0 +1,11 @@
+let message = 'Just learn it';
+
+function sendMessage (name) {
+	const sender = 'Gromcode';
+	console.log(`${name}, ${message}! Your ${sender}`);
+}
+
+
+function setMessage (text) {
+	message = text;
+}

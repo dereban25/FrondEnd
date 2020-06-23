cc793de980d4da84f1e3b9c61d42a01c94b52b23
Maksym Derbenev <dereban25@gmail.com>
2020-06-23 11:06:45 +0300

    new solution

---
+++ JavaScript/Lesson_14/task_3/index.js
@@ -0,0 +1,19 @@
+let result = 0;
+
+function add(num) {
+	result += num;
+}
+
+function decrease(num) {
+	result -= num;
+}
+
+function reset() {
+	result = 0;
+}
+
+function getMemo() {
+	return result;
+}
+
+export { add, decrease, reset, getMemo };

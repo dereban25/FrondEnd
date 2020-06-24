44c2c3c2eecc8825c461ad1f92a63daebc67dfc3
Maksym Derbenev <dereban25@gmail.com>
2020-06-24 10:23:39 +0300

    new solution

---
+++ JavaScript/Lesson_16/task_3/index.js
@@ -0,0 +1,17 @@
+function createArrayOfFunctions(num) {
+	
+	if (num === undefined) return [];
+	if (typeof num !== 'number') return null;
+	
+	const arr = [];
+	
+	for (let i = 0; i < num; i++) {
+		arr[i] = function() {
+			return i;
+		}
+	}
+	
+	return arr;
+}
+
+export { createArrayOfFunctions };

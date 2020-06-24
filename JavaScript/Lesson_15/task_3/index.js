dbb813bfa23ae437727b93a29ea683dc3fcae983
Maksym Derbenev <dereban25@gmail.com>
2020-06-24 09:59:21 +0300

    new solution

--- JavaScript/Lesson_15/task_3/index.js
+++ JavaScript/Lesson_15/task_3/index.js
@@ -1,53 +1,56 @@
-export function createLogger() {
-	let logger = [];
+export function createLogger () {
+	let records = [];
+	
+	function warn(text) {
+		return records.push({
+			message: text,
+			dateTime: new Date(),
+			type: 'warn' })
+	}
+	
+	function error(text) {
+		return records.push({
+			message: text,
+			dateTime: new Date(),
+			type: 'error' })
+	}
+	
+	function log(text) {
+		return records.push({
+			message: text,
+			dateTime: new Date(),
+			type: 'log' })
+	}
+	
+	function getRecords (text) {
+		switch (text) {
+			case 'worn':
+				return records
+					.filter(el => el.type === 'worn')
+					.sort((a, b) => b.dateTime - a.dateTime);
+			
+			case 'log':
+				return records
+					.filter(el => el.type === 'log')
+					.sort((a, b) => b.dateTime - a.dateTime);
+			
+			case 'error':
+				return records
+					.filter(el => el.type === 'error')
+					.sort((a, b) => b.dateTime - a.dateTime);
+			
+			case undefined:
+				return records
+					.sort((a, b) => b.dateTime - a.dateTime);
+			
+		}
+		return records;
+	}
 	
 	return {
-		warn: function(str) {
-			logger.push({
-				message: str,
-				dateTime: new Date(),
-				type: 'warn',
-			});
-		},
-		error: function(str) {
-			logger.push({
-				message: str,
-				dateTime: new Date(),
-				type: 'error',
-			});
-		},
-		log: function(str) {
-			logger.push({
-				message: str,
-				dateTime: new Date(),
-				type: 'log',
-			});
-		},
-		getRecords: function(str) {
-			switch (str) {
-				case 'worn':
-					return records
-						.filter(el => el.type === 'worn')
-						.sort((a, b) => b.dateTime - a.dateTime);
-				
-				case 'log':
-					return records
-						.filter(el => el.type === 'log')
-						.sort((a, b) => b.dateTime - a.dateTime);
-				
-				case 'error':
-					return records
-						.filter(el => el.type === 'error')
-						.sort((a, b) => b.dateTime - a.dateTime);
-				
-				case undefined:
-					return records
-						.sort((a, b) => b.dateTime - a.dateTime);
-				
-			}
-			return records;
-		}
-		
+		warn,
+		error,
+		log,
+		getRecords,
 	};
 }
-

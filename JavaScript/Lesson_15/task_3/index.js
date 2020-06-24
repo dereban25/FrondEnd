dbb4fa31331541ee4805bf33c3773dc304655100
Maksym Derbenev <dereban25@gmail.com>
2020-06-24 09:54:41 +0300

    new solution

--- JavaScript/Lesson_15/task_3/index.js
+++ JavaScript/Lesson_15/task_3/index.js
@@ -1,45 +1,35 @@
-
 export function createLogger() {
-	
-	let loggerArr = [];
-	
-	function warn(text) {
-		
-		loggerArr.push({
-			message: text,
-			dateTime: new Date(),
-			type: 'warn'
-		});
-	};
-	
-	function error(text) {
-		
-		loggerArr.push({
-			message: text,
-			dateTime: new Date(),
-			type: 'error'
-		});
-	};
-	
-	function log(text) {
-		
-		loggerArr.push({
-			message: text,
-			dateTime: new Date(),
-			type: 'log'
-		});
-		
-	};
-	
-	function getRecords(type) {
-		return loggerArr.sort((acc, rec) => (acc.dateTime > rec.dateTime));
-	};
+	let logger = [];
 	
 	return {
-		warn,
-		error,
-		log,
-		getRecords,
-	}
-};
+		warn: function(str) {
+			logger.push({
+				message: str,
+				dateTime: new Date(),
+				type: 'warn',
+			});
+		},
+		error: function(str) {
+			logger.push({
+				message: str,
+				dateTime: new Date(),
+				type: 'error',
+			});
+		},
+		log: function(str) {
+			logger.push({
+				message: str,
+				dateTime: new Date(),
+				type: 'log',
+			});
+		},
+		getRecords: function(str) {
+			if (!str) {
+				return logger;
+			}
+			return logger.filter(el => el.type === str).sort((a, b) => b.dateTime.getTime() - a.dateTime.getTime());
+		}
+		
+	};
+}
 

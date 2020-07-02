6f08bb679b80ea8e8eae5fd4a2cc5ed1827f7533
Maksym Derbenev <dereban25@gmail.com>
2020-07-02 12:45:52 +0300

    new solution

--- JavaScript/Lesson_17/task_9/index.js
+++ JavaScript/Lesson_17/task_9/index.js
@@ -1,5 +1,5 @@
 function calculator(a, b) {
-	switch (this.operation) {
+	switch(this.operation) {
 		case '+':
 			return a + b;
 		case '-':
@@ -12,21 +12,31 @@ function calculator(a, b) {
 			return NaN;
 	}
 }
+
 /*
  * Ф-ция multiplier должна быть создана на основе calculator
  * с использования .bind
  * и должна принимать 2 числа и возвращать из произведение
  */
 export const multiplier = calculator.bind({ operation: '*' });
+console.log(multiplier(2, 3));
+
+//const func = user.getFullName.bind(user);
+
+// func();
+
 /*
  * Ф-ция summator должна быть создана на основе calculator
  * с использования .bind
  * и должна принимать 2 числа и возвращать из сумму
  */
 export const summator = calculator.bind({ operation: '+' });
+console.log(summator(2, 3));
+
 /*
  * Ф-ция twice должна быть создана на основе calculator
  * с использования .bind
  * и должна принимать 1 число и возвращать это число умноженное на 2
  */
-export const twice = calculator.bind(mult, 2);
+export const twice = calculator.bind({ operation: '*' }, (calculator.b = 2));
+console.log(twice(3));

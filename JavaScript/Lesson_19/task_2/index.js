25cd7acd58162109862e03ac04dd27ae974d24b1
Maksym Derbenev <dereban25@gmail.com>
2020-07-04 21:14:05 +0300

    new solution

--- JavaScript/Lesson_19/task_2/index.js
+++ JavaScript/Lesson_19/task_2/index.js
@@ -2,7 +2,7 @@ export const getOwnProps = (obj) => {
 	const arr = [];
 	
 	// eslint-disable-next-line no-restricted-syntax
-	for (let a in obj) {
+	for (const a in obj) {
 		
 		if (obj.hasOwnProperty(a) && typeof obj[a] !== 'function') {
 			arr.push(a);

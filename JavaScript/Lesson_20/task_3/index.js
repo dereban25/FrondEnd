ced55cd3fc1a323a291530a30e56bb331bd6d1b3
Maksym Derbenev <dereban25@gmail.com>
2020-07-05 15:14:01 +0300

    new solution

---
+++ JavaScript/Lesson_20/task_3/index.js
@@ -0,0 +1,23 @@
+class Wallet {
+	#balance = 0;
+	
+	getBalance() {
+		return this.#balance;
+	}
+	
+	deposit(amount) {
+		this.#balance += amount;
+	}
+	
+	withdraw(amount) {
+		if (amount > this.#balance) {
+			console.log('No enough funds');
+			return false;
+		}
+		
+		this.#balance -= amount;
+	}
+}
+
+
+export {Wallet};

300364bc73ba0e87879bf07defa8bf0da7330f8b
Maksym Derbenev <dereban25@gmail.com>
2020-07-05 13:35:43 +0300

    new solution

---
+++ JavaScript/Lesson_20/task_1/index.js
@@ -0,0 +1,35 @@
+
+class User {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	sayHi() {
+		console.log(`Hi, I am ${this.name}`);
+	};
+	
+	requestNewPhoto() {
+		console.log(`New photo request was sent for ${this.name}`);
+	};
+	
+	setAge(value) {
+		if (value < 0)
+			return false;
+		
+		this.age = value;
+		
+		if (value >= 25)
+			this.requestNewPhoto();
+		
+		return value;
+	};
+	
+	static createEmpty() {
+		return new this('', null);
+	}
+};
+
+
+
+export { User };

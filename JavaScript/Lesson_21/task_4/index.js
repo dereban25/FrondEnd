7ebbf7c9d4736aeb25ab55494f780be68ac9792c
Maksym Derbenev <dereban25@gmail.com>
2020-07-07 20:35:06 +0300

    new solution

--- JavaScript/Lesson_21/task_4/index.js
+++ JavaScript/Lesson_21/task_4/index.js
@@ -2,21 +2,21 @@ export const getTitle = body => {
 	return (document.querySelector('.title')).textContent;
 };
 
-console.log(getTitle());
+// console.log(getTitle());
 
 export const getDescription = body => {
 	return (document.querySelector('.about')).innerText;
 };
 
-console.log(getDescription());
+// console.log(getDescription());
 
 export const getPlans = body => {
 	return (document.querySelector('.plans')).innerHTML;
 }
 
-console.log(getPlans());
+// console.log(getPlans());
 
 export const getGoal = body => {
 	return (document.querySelector('.goal')).outerHTML;
 }
-console.log(getGoal());
+// console.log(getGoal());
